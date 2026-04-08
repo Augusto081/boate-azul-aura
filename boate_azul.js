b156c7db2d101c164cae270982ce3ec611df8e26
Gabriel Augusto <jacarepoio0912@gmail.com>
2026-04-08 17:05:19 -0300

    Add files via upload

---
+++ boate_azul.js
@@ -0,0 +1,76 @@
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  async function cantar() {
+    console.clear();
+    console.log("🎶 BOATE AZUL🎶\n");
+    await delay(1500);
+  
+    console.log("💔 Doente de amor");
+    await delay(2500);
+  
+    console.log("🌙 Procurei remédio na vida noturna");
+    await delay(2500);
+  
+    console.log("🍷 Como uma flor da noite");
+    await delay(2500);
+  
+    console.log("\n🍸 Em uma bote aqui na Zona Sul");
+    await delay(2500);
+  
+    console.log("😎A dor do amor");
+    await delay(2500);
+  
+    console.log("🙏 É com outro amor que a gente cura");
+    await delay(2500);
+  
+    console.log("\n🎤 Vim curar a dor");
+    await delay(2500);
+  
+    console.log("💭 Deste mal de amor na boate azul");
+    await delay(2500);
+  
+    console.log("😔 E quando a noite vai se agonizando no clarão da aurora");
+    await delay(2500);
+  
+    console.log("\n💡 Os integrantes da vida noturna se foram dormir");
+    await delay(2500);
+  
+    console.log("😶 E a dama da noite que estava comigo também foi embora");
+    await delay(2500);
+  
+    console.log("💔 Fecharam-se as portas");
+    await delay(2500);
+  
+    console.log("\n🔁 Sozinho de novo tive que sair");
+    await delay(2000);
+  
+    console.log("🍷 Sair de que jeito");
+    await delay(2500);
+  
+    console.log("😵‍💫 Se nem sei o rumo para onde vou");
+    await delay(2500);
+  
+    console.log("🙏 Muito vagamente me lembro que estou");
+    await delay(2500);
+  
+    console.log("💔 Em uma boate aqui na Zona Sul");
+    await delay(2500);
+  
+    console.log("\n🎶 Eu bebi demais");
+    await delay(3000);
+  
+    console.log("\n🌅 E não consigo me lembrar sequer");
+    await delay(2500);
+  
+    console.log("🥀 Qual era o nome daquela mulher");
+    await delay(2500);
+  
+    console.log("💭 A flor da noite da boate azul");
+    await delay(2500);
+  
+    console.log("\n🎤 FIM DA AURA RSRS");
+  }
+  
+  cantar();
