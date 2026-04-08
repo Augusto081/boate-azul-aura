8da020f275b50cf5c8a7b5bbc79c650c5196da6c
Gabriel Augusto <jacarepoio0912@gmail.com>
2026-04-08 17:08:00 -0300

    Add files via upload

--- boate_azul.js
+++ boate_azul.js
@@ -4,73 +4,73 @@ function delay(ms) {
   
   async function cantar() {
     console.clear();
-    console.log("🎶 BOATE AZUL🎶\n");
+    console.log(" BOATE AZUL\n");
     await delay(1500);
   
-    console.log("💔 Doente de amor");
+    console.log(" Doente de amor");
     await delay(2500);
   
-    console.log("🌙 Procurei remédio na vida noturna");
+    console.log(" Procurei remédio na vida noturna");
     await delay(2500);
   
-    console.log("🍷 Como uma flor da noite");
+    console.log(" Como uma flor da noite");
     await delay(2500);
   
-    console.log("\n🍸 Em uma bote aqui na Zona Sul");
+    console.log("\n Em uma bote aqui na Zona Sul");
     await delay(2500);
   
-    console.log("😎A dor do amor");
+    console.log(" A dor do amor");
     await delay(2500);
   
-    console.log("🙏 É com outro amor que a gente cura");
+    console.log(" É com outro amor que a gente cura");
     await delay(2500);
   
-    console.log("\n🎤 Vim curar a dor");
+    console.log("\n Vim curar a dor");
     await delay(2500);
   
-    console.log("💭 Deste mal de amor na boate azul");
+    console.log(" Deste mal de amor na boate azul");
     await delay(2500);
   
-    console.log("😔 E quando a noite vai se agonizando no clarão da aurora");
+    console.log(" E quando a noite vai se agonizando no clarão da aurora");
     await delay(2500);
   
-    console.log("\n💡 Os integrantes da vida noturna se foram dormir");
+    console.log("\n Os integrantes da vida noturna se foram dormir");
     await delay(2500);
   
-    console.log("😶 E a dama da noite que estava comigo também foi embora");
+    console.log(" E a dama da noite que estava comigo também foi embora");
     await delay(2500);
   
-    console.log("💔 Fecharam-se as portas");
+    console.log(" Fecharam-se as portas");
     await delay(2500);
   
-    console.log("\n🔁 Sozinho de novo tive que sair");
+    console.log("\n Sozinho de novo tive que sair");
     await delay(2000);
   
-    console.log("🍷 Sair de que jeito");
+    console.log(" Sair de que jeito");
     await delay(2500);
   
-    console.log("😵‍💫 Se nem sei o rumo para onde vou");
+    console.log(" Se nem sei o rumo para onde vou");
     await delay(2500);
   
-    console.log("🙏 Muito vagamente me lembro que estou");
+    console.log(" Muito vagamente me lembro que estou");
     await delay(2500);
   
-    console.log("💔 Em uma boate aqui na Zona Sul");
+    console.log(" Em uma boate aqui na Zona Sul");
     await delay(2500);
   
-    console.log("\n🎶 Eu bebi demais");
+    console.log("\n Eu bebi demais");
     await delay(3000);
   
-    console.log("\n🌅 E não consigo me lembrar sequer");
+    console.log("\n E não consigo me lembrar sequer");
     await delay(2500);
   
-    console.log("🥀 Qual era o nome daquela mulher");
+    console.log(" Qual era o nome daquela mulher");
     await delay(2500);
   
-    console.log("💭 A flor da noite da boate azul");
+    console.log(" A flor da noite da boate azul");
     await delay(2500);
   
-    console.log("\n🎤 FIM DA AURA RSRS");
+    console.log("\n FIM DA AURA RSRS");
   }
   
   cantar();
